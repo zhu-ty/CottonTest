7180ace6ace3582e2f3cd59c7dec15fd5dd25203
zhu-ty <zhu-ty-temp@outlook.com>
2016-12-15 09:45:37 +0800

    update average

--- back/Serial/Serial/main.cpp
+++ back/Serial/Serial/main.cpp
@@ -7,26 +7,48 @@
 
 #define SERVER_MODE_ENABLE
 
-int cap_num = 0;
+typedef unsigned int uint;
+
+uint cap_num = 0;
 std::shared_ptr<mutex> mMutex;
 std::shared_ptr<RawDataPack> data_pack;
 int CapCallBack(PvImage* pData, void* pUserData);
 
 
-unsigned int ByteToshort(unsigned char * bytes)
+uint ByteToshort(unsigned char * bytes)
 {
-	unsigned int i = 0;
-	i += (unsigned int)(bytes[0]);
-	i += ((unsigned int)(bytes[1]) << 8) & 0xff00;
+	uint i = 0;
+	i += (uint)(bytes[0]);
+	i += ((uint)(bytes[1]) << 8) & 0xff00;
 	return i;
 }
 
-void shortToByte(unsigned int i, unsigned char * bytes)
+void shortToByte(uint i, unsigned char * bytes)
 {
 	memset(bytes, 0, sizeof(unsigned char) * 2);
 	bytes[0] = (unsigned char)(0xff & i);
 	bytes[1] = (unsigned char)((0xff00 & i) >> 8);
 	return;
+}
+
+void avg_function(unsigned char *data, unsigned char *avg_ans, uint avg_count)
+{
+	uint to_avg[RAW_DATA_LENTH / 2] = { 0 };
+	memset(to_avg, 0, sizeof(uint) * RAW_DATA_LENTH / 2);
+	for (uint i = 0; i < avg_count; i++)
+	{
+		for (uint j = 0; j < RAW_DATA_LENTH / 2; j++)
+		{
+			to_avg[j] += ByteToshort(data + i * RAW_DATA_LENTH + j * 2);
+		}
+	}
+	for (uint j = 0; j < RAW_DATA_LENTH / 2; j++)
+	{
+		unsigned char tmp[2];
+		to_avg[j] = to_avg[j] / avg_count;
+		shortToByte(to_avg[j], tmp);
+		memcpy(avg_ans + j * 2, tmp, 2);
+	}
 }
 
 int main()
@@ -88,27 +110,40 @@ int CapCallBack(PvImage* pData, void* pUserData)
 		printf("\n");
 	}
 	mMutex->lock();
-	unsigned int real_avg = max(min(80 * 2 * 100 / RAW_DATA_LENTH, data_pack->avg),1);
+	uint real_avg = max(min(80 * 2 * 100 / RAW_DATA_LENTH, data_pack->avg),1);
 	mMutex->unlock();
-	unsigned int to_avg[RAW_DATA_LENTH / 2] = { 0 };
-	memset(to_avg, 0, sizeof(unsigned int) * RAW_DATA_LENTH / 2);
-	for (int i = 0; i < real_avg; i++)
-	{
-		for (int j = 0; j < RAW_DATA_LENTH / 2; j++)
-		{
-			to_avg[j] += ByteToshort(lDataPtr + i * RAW_DATA_LENTH + j * 2);
-		}
-	}
 	unsigned char avg_ans[RAW_DATA_LENTH];
-	for (int j = 0; j < RAW_DATA_LENTH / 2; j++)
-	{
-		unsigned char tmp[2];
-		to_avg[j] = to_avg[j] / real_avg;
-		shortToByte(to_avg[j], tmp);
-		memcpy(avg_ans + j * 2, tmp, 2);
-	}
+
+	avg_function(lDataPtr, avg_ans, real_avg);
 	mMutex->lock();
 	memcpy(data_pack->data, avg_ans, RAW_DATA_LENTH);
 	mMutex->unlock();
 	return true;
 }
+
+
+/*
+
+unsigned char x[] = {
+0x5D,0x21,0x5D,0x21,
+0x5D,0x21,0x5D,0x21,
+0x5D,0x21,0x5D,0x21,
+
+0xFA,0x04,0xFA,0x04,
+0xFA,0x04,0xFA,0x04,
+0xFA,0x04,0xFA,0x04,
+
+0xBC,0x19,0xBC,0x19,
+0xBC,0x19,0xBC,0x19,
+0xBC,0x19,0xBC,0x19,
+};
+unsigned char aaaa[12] = { 0 };
+avg_function(x, aaaa, 3);
+for (int i = 0; i < 12; i ++ )
+{
+printf("%#02x ", aaaa[i]);
+}
+
+system("pause");
+return 0;
+*/
